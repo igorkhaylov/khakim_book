98644bc9c0d9fc03eb9927c95d9c760179377c1f
igorkhaylov <sonikry.99@gmail.com>
2022-01-11 21:12:38 +0500

    fix

--- khakim_book/settings.py
+++ khakim_book/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-i_fsz@w+wucw--1%jiwi)vvdd%bpohx-bncak%cx+eq1^5@t!q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -120,8 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.core.mail import send_mail
 
 
 def index(request):
-    return render(request, "index.html",)
+    return render(request, "index.html", )
 
 
 def send_message(request):
@@ -15,11 +15,14 @@ def send_message(request):
         # card_number = request.POST['card_number']
         print(name, phone_number, email, sep='\n')
         message = f'Имя: {name}, \nНомер телефона: {phone_number},\nE-mail: {email},'
-        mail = send_mail("Khakim Book", message, 'libertycode01@gmail.com', ['igorkhaylov@yandex.com', ],
-                         fail_silently=False, )
+        mail = send_mail("Khakim Book",
+                         message,
+                         'libertycode01@gmail.com',
+                         ['libertycode01@gmail.com', 'korolt073@gmail.com', ],
+                         fail_silently=False,
+                         )
         if mail:
             print("Сообщение успешно отправлено")
         else:
             print("Ошибка отправки сообщения")
     return HttpResponseRedirect('/')
-
